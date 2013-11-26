03bef45718c5468366e7411e1f75e71c2471f7fa
FenilShah <shahfenil07@gmail.com>
2013-11-26 20:38:52 +0530

    Recieve mail example

--- FoodCart/src/com/test.java
+++ FoodCart/src/com/test.java
@@ -1,57 +1,61 @@
 package com;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
+import java.util.Properties;
+
+import javax.mail.Folder;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.NoSuchProviderException;
+import javax.mail.Session;
+
+import com.sun.mail.pop3.POP3Store;
 
 public class test {
-	public static void main(String[] args) throws IOException 
-	{
-		BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\fshah\\Desktop\\TS.txt"));
-	    try {
-	       // StringBuilder sb = new StringBuilder();
-	        String line = br.readLine();
-	        
-	        boolean flag = false;;
-	        Integer lines = Integer.parseInt(line);
-	        for(int i = 0; i<lines; i++){
-	        	line = br.readLine();
-	        	System.out.println(line);
-	        	int fOcc = line.indexOf("#");
-	        	//System.out.println(line.indexOf("#"));
-	        	if(fOcc>0){
-	        		char c[] = line.toCharArray();
-	        		int occCount = 1;
-	        		String check = "#";
-	        		for(int j=fOcc+1;j<line.length();j++){
-	        			if('#' == c[j]){
-	        				occCount++;
-	        				check += "#";
-	        			}
-	        		}
-	        		
-	        		if(occCount > 1){
-	        			flag = true;
-	        			for(int k=i+1;k<lines && k < i+occCount;k++){
-	        				line = br.readLine();
-	        				System.out.println(line);
-	        				String j = line.substring(fOcc,fOcc + occCount);
-	        				if(!j.equals(check))
-	        					flag = false;
-	        			}
-	        			break;
-	        		}
-	        	}
-	        	
-	        }
-	        
-	        if(flag)
-	        	System.out.println("Square");
-	        else
-	        	System.out.println("Not Square");
-	    } finally {
-	        br.close();
-	    }
+
+	public static void main(String[] args) {
+
+		String mailPop3Host = "surya.ismnet.com";
+		String mailStoreType = "pop3";
+		String mailUser = "fshah";
+		String mailPassword = "hellboy123";
+
+		receiveEmail(mailPop3Host, mailStoreType, mailUser, mailPassword);
 	}
+
+	public static void receiveEmail(String pop3Host, String storeType, String user, String password) {
+
+		try {
+			Properties properties = new Properties();
+			properties.put("mail.pop3.host", pop3Host);
+			Session emailSession = Session.getDefaultInstance(properties);
+
+			POP3Store emailStore = (POP3Store) emailSession.getStore(storeType);
+			emailStore.connect(user, password);
+
+			Folder emailFolder = emailStore.getFolder("INBOX");
+			emailFolder.open(Folder.READ_ONLY);
+
+			Message[] messages = emailFolder.getMessages();
+			for (int i = 0; i < messages.length; i++) {
+				Message message = messages[i];
+				System.out.println("==============================");
+				System.out.println("Email #" + (i + 1));
+				System.out.println("Subject: " + message.getSubject());
+				System.out.println("From: " + message.getFrom()[0]);
+				System.out.println("Text: " + message.getContent().toString());
+			}
+
+			emailFolder.close(false);
+			emailStore.close();
+		} catch (NoSuchProviderException e) {
+			e.printStackTrace();
+		} catch (MessagingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
 }
